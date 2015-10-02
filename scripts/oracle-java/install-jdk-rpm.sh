dcf37b45a5e724e47bb13947ab8e90dac9008e92
Jorge Quilcate <quilcate.jorge@gmail.com>
2015-10-02 15:44:04 +0200

    fix json in progrss

--- scripts/oracle-java/install-jdk-rpm.sh
+++ scripts/oracle-java/install-jdk-rpm.sh
@@ -3,5 +3,5 @@
 rpm -i $JAVA_RPM
 rm $JAVA_RPM
 
-echo "export JAVA_HOME=/usr/java/default" > /etc/profile.d/jdk.sh
-echo "export PATH=${JAVA_HOME}/bin:${PATH}" >> /etc/profile.d/jdk.sh
+echo 'export JAVA_HOME=$JAVA_HOME' > /etc/profile.d/jdk.sh
+echo 'export PATH=${JAVA_HOME}/bin:${PATH}' > /etc/profile.d/jdk.sh
